f1c338d7a10a5c00ae5ffd8dda71e45f0e577cc6
oxyesthesia <166774931+oxyesthesia@users.noreply.github.com>
2024-05-24 17:58:25 +1000

    new code lets gooooo

--- script.js
+++ script.js
@@ -1,40 +1,85 @@
-// Create an object called 'task'
-  // Populate the properties based on the provided data model
-// Create a function called 'addTask'
-  // Give the function input parameters for: name, type, rate, time, client
-  // Paste your object definition from above in the function
-  // Replace the property values with the input paramaters
-  // Add the object to the taskList array
+const form = document.getElementById("filmform");
+const filmlist = document.getElementById("filmlist");
 
-class Task {
-  constructor(name ,type, id, date, rate, time, client) {
+function openForm() {
+  document.getElementById('filmform').style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById('filmform').style.display = "none";
+}
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  addFilm(form.elements.filmName.value, form.elements.filmGenre.value, form.elements.filmRating.value, form.elements.filmLength.value, form.elements.filmRelease.value, form.elements.filmScore.value, form.elements.filmWatch.value);
+  console.log(filmList);
+  document.getElementById('filmform').style.display = "none";
+})
+
+function displayFilm(film) {
+  let item = document.createElement("li");
+  item.setAttribute("data-id", film.id);
+  item.innerHTML =`<p><strong>${film.name}</strong><br>${film.genre}<br>${film.rating}<br>${film.length}<br>${film.release}<br>${film.score}<br>${film.watch}</p>`;
+  filmlist.appendChild(item);
+  form.reset();
+  // Setup delete button DOM elements
+  let delButton = document.createElement("button");
+  let delButtonText = document.createTextNode("Delete");
+  delButton.appendChild(delButtonText);
+  item.appendChild(delButton); // Adds a delete button to every task
+  // Listen for when the delete button is clicked
+  delButton.addEventListener("click", function(event) {
+    item.remove(); // Remove the task item from the page when button clicked
+    // Because we used 'let' to define the item, this will delete the right element
+    
+    // Filter out the element corresponding with the list item and store the new task list
+    filmList = filmList.filter( film => film.id != item.getAttribute('data-id') )
+  
+    // Make sure the deletion worked by logging out the whole array
+    // console.log(filmList)
+  })
+}
+
+class Film {
+  constructor(name, genre, rating, length, release, score, watch, id, date) {
     this.name = name;
-    this.type = type;
+    this.genre = genre;
+    this.rating = rating;
+    this.length = length;
+    this.release = release;
+    this.score = score;
+    this.watch = watch;
     this.id = id;
     this.date = date;
-    this.rate = rate;
-    this.time = time;
-    this.client = client;
   }
 }
 
-taskList = [];
-
-function addTask(name, type, rate, time, client) {
-  var newTask = new Task(name, type, Date.now(), new Date().toISOString(), rate, time, client);
-  taskList.push(newTask);
+function storeData(entry) {
+  const filmListMaster = JSON.parse(localStorage.getItem('filmListMaster')) || [];
+  filmListMaster.push(entry);
+  localStorage.setItem('filmListMaster', JSON.stringify(filmListMaster));
 }
 
-addTask("Initial Sketches", "Concept Ideation", 30, 2, "Google");
-addTask("Home page design", "Wireframe Design", 45, 5, "Atlassian");
-addTask("Backend dev", "Application Coding", 80, 12, "Windows");
-addTask("Database testing", "Testing/Debugging", 60, 15, "Amazon");
-addTask("User research", "Pitch deck", 50, 38, "Woolworths");
+function loadData() {
+  const filmListMaster = JSON.parse(localStorage.getItem('filmListMaster')) || [];
+  filmListMaster.forEach(task => {
+    console.log(task);
+  });
+}
 
-console.log(taskList);
+// localStorage.clear();
 
-// Call the function with test values for the input paramaters
+filmList = [];
 
+function addFilm(name, genre, rating, length, release, score, watch) {
+  var newFilm = new Film(name, genre, "resources/Classification" + rating + ".png", length, release, score, watch, new Date().toISOString(), Date.now(),);
+  filmList.push(newFilm);
+  displayFilm(newFilm);
+  // storeData(filmList);
+  loadData();
+}
 
-
-// Log the array to the console.
+addFilm("Kill Bill", "Action", "M", 108, 2012, 4, "12/4/24");
+// localStorage.setItem('filmList', JSON.stringify(filmList));
+// console.log(filmList);
+// updateList();
