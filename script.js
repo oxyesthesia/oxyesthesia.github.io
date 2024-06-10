bb8c1e57f9547cf7226ae6b1651cfd369d40df11
oxyesthesia <166774931+oxyesthesia@users.noreply.github.com>
2024-06-10 11:28:08 +1000

    Comments for script.js

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 const form = document.getElementById("filmform");
 const filmlist = document.getElementById("filmlist");
-sortvalue = "date"
+//This value defaults to 'date', but will change when the user selects one of the sorting buttons. 
+//This way, the list will rewrite itself based on whatever the user last picked (unless they refresh the page)
+sortvalue = "date" 
 
+// The following is activated when the 'Add A Film' button is selected. It brings up the form and the grey background, and prevents scrolling
 function openForm() {
   document.getElementById('filmform').style.display = "block";
   document.getElementById('translucent').style.display = "block";
   document.body.classList.add("stop-scrolling");
 }
 
+// Activated when a user selects the 'Close' button on the form. Hides the form and grey background, and enables scrolling.
 function closeForm() {
   document.getElementById('filmform').style.display = "none";
   document.getElementById('translucent').style.display = "none";
   document.body.classList.remove("stop-scrolling");
 }
 
+// Activated when data is successfully submitted through the form. It creates an object with the film data, hides the form/grey background and enables scrolling.
 form.addEventListener("submit", function(event) {
   event.preventDefault();
   addFilm(form.elements.filmName.value, form.elements.filmGenre.value, form.elements.filmRating.value, form.elements.filmLength.value, form.elements.filmRelease.value, form.elements.filmScore.value, form.elements.filmWatch.value);
@@ -22,6 +27,8 @@ form.addEventListener("submit", function(event) {
   document.body.classList.remove("stop-scrolling");
 })
 
+//This code is a modification of the class work in Week 4 - Interactive Tasklist. Instead of tasks, it's movies! Pulls the list of objects from 
+// the filmListMaster (which is stored in the localStorage) and individually appends them to the empty list in the html.
 function displayFilm(film) {
   let item = document.createElement("li");
   item.setAttribute("data-id", film.id);
@@ -29,6 +36,7 @@ function displayFilm(film) {
   filmlist.appendChild(item);
   form.reset();
 
+  // Creates the individual 'delete' buttons for each data entry. Again, taken from the Interactive Tasklist class task.
   let delButton = document.createElement("button");
   delButton.setAttribute('id', 'delete');
   let delButtonText = document.createTextNode("Delete");
@@ -39,6 +47,7 @@ function displayFilm(film) {
   })
 }
 
+// Here's where each film's data get processed into an object, used in the addFilm() function. 
 class Film {
   constructor(name, genre, rating, length, release, score, watch, id, date) {
     this.name = name;
@@ -53,6 +62,8 @@ class Film {
   }
 }
 
+// Thus function runs every time there is an addition to the film list. Adds the new data to the end of the array
+// (maintaining the 'Entry Date' sorting order) and then rewrites the list to display the new addition.
 function storeData(entry) {
   const filmListMaster = JSON.parse(localStorage.getItem('filmListMaster')) || [];
   filmListMaster.push(entry);
@@ -60,6 +71,8 @@ function storeData(entry) {
   loadData();
 }
 
+// This function rewrites the film list onto the page. It firstly wipes the page of all items, then adds each item
+// to a newly-made array thanks to the toSorted() function based on which sorting method was last activated.
 function loadData() {
   const filmListMaster = JSON.parse(localStorage.getItem('filmListMaster')) || [];
   document.getElementById("filmlist").innerHTML = "";
@@ -83,6 +96,8 @@ function loadData() {
     displayFacts(filmListMaster);
 }
 
+// This function activates when a 'Delete' button has been pressed, and rifles through the stored array to find the matching
+// entry based on the id value. Once a match is found, it is spliced from the array and the array is rewritten from loadData().
 function removeData(entry) {
   const filmListMaster = JSON.parse(localStorage.getItem('filmListMaster')) || [];
   filmListMaster.forEach(task => {
@@ -97,11 +112,15 @@ function removeData(entry) {
   loadData();
 }
 
+// This one defines the sorting type, then rewrites the film list according to the sorting type.
 function sort(type) {
   sortvalue = type;
   loadData();
 }
 
+// The next six functions are used in the toSorted() function to compare the array objects based on the defined sorting
+// property. Most of them are identical, except for Score which orders them from highest to lowest, and Rating which requires
+// a comparison of an array index, as the ratings are not ordered alphabetically.
 function compareName(a, b) {
   if(a.name < b.name){
     return -1;
@@ -151,6 +170,10 @@ function compareDate(a, b) {
   } return 0;
 }
 
+// This function finds the information for the fun facts that are displayed on the left-hand side of the page. Almost
+// all of the math is in this function, except for the getMode() function which proved to be too much to incorporate
+// into this function. The top half of the function grabs the datasets and adds then to variables which are then
+// used in the second half of the function to calculate and display on the webpage.
 function displayFacts(films) {
   var totalFilms = 0;
   var totalTime = 0;
@@ -200,12 +223,9 @@ function displayFacts(films) {
   factlist.append(item5);
 }
 
-// This function below will find the mode(s) of the favGenre array.
-//Big help from CodeTour on YouTube for this function, I only modified the ending to
-//provide multiple answers if there were more than one top film genre.
-
-//https://www.youtube.com/watch?v=0V2Mi16xd04
-
+// This function will find the mode(s) of the favGenre array. Big help from CodeTour on YouTube 
+// for this function, I only modified the ending to provide multiple answers if there were more than one 
+// top film genre. https://www.youtube.com/watch?v=0V2Mi16xd04
 function getMode(array) { 
   const obj = {};
   array.forEach(item => {
@@ -238,13 +258,19 @@ function getMode(array) {
   return ans;
 }
 
+// To load the data stored in localStorage upon page (re)load, the loadData() function is ran initially.
 loadData();
 
+// This function takes the information from the form (or the fillList() function below if using dev buttons) and
+// creates an object for that film. It then adds runs the storeData() function, passing in that object so that
+// it is added to the filmListMaster array, and therefore the localStorage.
 function addFilm(name, genre, rating, length, release, score, watch) {
   var newFilm = new Film(name, genre, "resources/Classification" + rating + ".png", length, release, score, watch, Math.floor(Math.random() * 10000), Date.now());
   storeData(newFilm);
 }
 
+// This function is the first of the dev buttons at the bottom of the webpage. It basically adds a bunch of
+// pre-made film details into the list for your experimentation. Yes, these are my personal scores of these films.
 function fillList() {
   addFilm("Kill Bill: Volume 1", "Action", "R18+", 93, 2003, 3, "12/4/18");
   addFilm("Scott Pilgrim Vs The World", "Action", "M", 112, 2010, 5, "9/9/10");
@@ -258,6 +284,8 @@ function fillList() {
   addFilm("Hot Fuzz", "Comedy", "MA15+", 121, 2007, 4, "3/12/16");
 }
 
+// This function is the second of the dev buttons at the bottom of the webpage. This one wipes the film list
+// entirely, allowing you to start fresh.
 function deleteList() {
   filmListMaster = JSON.parse(localStorage.getItem('filmListMaster')) || [];
   filmListMaster = [];
